982723e1ecb59d9bb222a1f342e6034255338928
Reed Esau <reed@esau.org>
2021-07-08 18:31:06 -0600

    removed standard asset descriptions to plan better for translation

--- Sources/Model/MAsset+StandardID.swift
+++ Sources/Model/MAsset+StandardID.swift
@@ -55,75 +55,76 @@ public extension MAsset {
         case tips = "TIPS"
         case total = "Total"
         
-        public var description: String {
-            switch self {
-            case .bond:
-                return "US Aggregate Bonds"
-            case .cash:
-                return "Cash"
-            case .cmdty:
-                return "Commodities"
-            case .corpbond:
-                return "US Corporate Bonds"
-            case .em:
-                return "Emerging Market Equities"
-            case .embond:
-                return "Emerging Market Bonds"
-            case .europe:
-                return "Europe Equities"
-            case .globre:
-                return "Global Real Estate"
-            case .gold:
-                return "Gold"
-            case .hybond:
-                return "High Yield Bonds"
-            case .intl:
-                return "International Equities"
-            case .intlbond:
-                return "International Bonds"
-            case .intlgov:
-                return "International Treasuries"
-            case .intlre:
-                return "International Real Estate"
-            case .intlsc:
-                return "International Small Cap Equities"
-            case .intlval:
-                return "International Value Equities"
-            case .itgov:
-                return "Intermediate Term US Treasuries"
-            case .japan:
-                return "Japan Equities"
-            case .lc:
-                return "US Large Cap Equities"
-            case .lcgrow:
-                return "US Large Cap Growth Equities"
-            case .lcval:
-                return "US Large Cap Value Equities"
-            case .ltgov:
-                return "Long-Term US Treasuries"
-            case .momentum:
-                return "US Momentum Equities"
-            case .pacific:
-                return "Pacific Equities"
-            case .re:
-                return "US Real Estate"
-            case .remort:
-                return "US Mortgage REITs"
-            case .sc:
-                return "US Small Cap Equities"
-            case .scgrow:
-                return "US Small Cap Growth Equities"
-            case .scval:
-                return "US Small Cap Value Equities"
-            case .stgov:
-                return "Short-Term US Treasuries"
-            case .tech:
-                return "US Technology Equities"
-            case .tips:
-                return "US Inflation Protected Treasuries"
-            case .total:
-                return "US Total Market"
-            }
-        }
+        //TODO not used yet. Need to be handled with locale
+//        public var description: String {
+//            switch self {
+//            case .bond:
+//                return "US Aggregate Bonds"
+//            case .cash:
+//                return "Cash"
+//            case .cmdty:
+//                return "Commodities"
+//            case .corpbond:
+//                return "US Corporate Bonds"
+//            case .em:
+//                return "Emerging Market Equities"
+//            case .embond:
+//                return "Emerging Market Bonds"
+//            case .europe:
+//                return "Europe Equities"
+//            case .globre:
+//                return "Global Real Estate"
+//            case .gold:
+//                return "Gold"
+//            case .hybond:
+//                return "High Yield Bonds"
+//            case .intl:
+//                return "International Equities"
+//            case .intlbond:
+//                return "International Bonds"
+//            case .intlgov:
+//                return "International Treasuries"
+//            case .intlre:
+//                return "International Real Estate"
+//            case .intlsc:
+//                return "International Small Cap Equities"
+//            case .intlval:
+//                return "International Value Equities"
+//            case .itgov:
+//                return "Intermediate Term US Treasuries"
+//            case .japan:
+//                return "Japan Equities"
+//            case .lc:
+//                return "US Large Cap Equities"
+//            case .lcgrow:
+//                return "US Large Cap Growth Equities"
+//            case .lcval:
+//                return "US Large Cap Value Equities"
+//            case .ltgov:
+//                return "Long-Term US Treasuries"
+//            case .momentum:
+//                return "US Momentum Equities"
+//            case .pacific:
+//                return "Pacific Equities"
+//            case .re:
+//                return "US Real Estate"
+//            case .remort:
+//                return "US Mortgage REITs"
+//            case .sc:
+//                return "US Small Cap Equities"
+//            case .scgrow:
+//                return "US Small Cap Growth Equities"
+//            case .scval:
+//                return "US Small Cap Value Equities"
+//            case .stgov:
+//                return "Short-Term US Treasuries"
+//            case .tech:
+//                return "US Technology Equities"
+//            case .tips:
+//                return "US Inflation Protected Treasuries"
+//            case .total:
+//                return "US Total Market"
+//            }
+//        }
     }
 }
